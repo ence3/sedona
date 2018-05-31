7aa9c11fe744b81d606458af01bffbe0e00aaff1
Nikita Shabanov <nktenc@gmail.com>
2018-05-31 15:39:30 +0500

    Мобильная версия завершена

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var navMain = document.querySelector ('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var navClose = document.querySelector('.site-list__item-btn');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function (){
+	if (navMain.classList.contains('main-nav--closed')){
+		navMain.classList.remove('main-nav--closed');
+		navMain.classList.add('main-nav--opened');
+	} else {
+		navMain.classList.add('main-nav--closed');
+		navMain.classList.remove('main-nav--opened');
+	}
+	});
+
+navClose.addEventListener ('click', function (){
+	if (navMain.classList.contains('main-nav--opened')){
+		navMain.classList.remove('main-nav--opened');
+		navMain.classList.add('main-nav--closed');
+	}
+	});
